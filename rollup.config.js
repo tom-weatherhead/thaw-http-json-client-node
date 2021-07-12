822a335a2be2123438db9ac939e12cfb1d91c5b1
Tom Weatherhead <tomw@2hrd4u.org>
2021-07-12 15:06:44 -0600

    Dev

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@
 
 // import externalGlobals from 'rollup-plugin-external-globals';
 import { terser } from 'rollup-plugin-terser';
-// rollup-plugin-node-polyfills
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
 	input: './dist/lib/main.js',
@@ -11,23 +12,29 @@ export default {
 			file: 'dist/thaw-http-json-client-node.cjs.js',
 			format: 'cjs',
 			exports: 'named',
-			globals: { rxjs: 'rxjs', http: 'http', https: 'https' }
+			// external: ['http', 'https'] // ,
+			globals: { http: 'http', https: 'https' }
 		},
 		{
 			file: 'dist/thaw-http-json-client-node.esm.js',
 			format: 'es',
 			compact: true,
-			globals: { rxjs: 'rxjs', http: 'http', https: 'https' }
-		},
-		{
-			file: 'dist/thaw-http-json-client-node.js',
-			name: 'thaw-http-json-client-node',
-			format: 'umd',
-			compact: true,
-			globals: { rxjs: 'rxjs', http: 'http', https: 'https' }
+			// external: ['http', 'https'] // ,
+			globals: { http: 'http', https: 'https' }
+		// },
+		// {
+		// 	file: 'dist/thaw-http-json-client-node.js',
+		// 	name: 'thaw-http-json-client-node',
+		// 	format: 'umd',
+		// 	compact: true,
+		// 	// external: ['http', 'https'] // ,
+		// 	globals: { http: 'http', https: 'https' }
 		}
 	],
+	external: ['http', 'https'],
 	plugins: [
+		nodePolyfills(),
+		nodeResolve({ preferBuiltins: false }),
 		terser() // ,
 		// externalGlobals({ rxjs: 'rxjs', http: 'http', https: 'https' })
 		// externalGlobals({ http: 'http', https: 'https' })
